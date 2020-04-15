98e063aa8e4aa6af1d8e83a6af45b8b330225c2a
web-oscol <61119187+web-oscol@users.noreply.github.com>
2020-04-15 16:18:59 +0300

    gaz

---
+++ gaz/js/3slede.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+$('#multiCarousel').carousel({
+  interval: false
+})
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+  });
+});
