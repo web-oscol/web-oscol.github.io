d848448c7b0084ade1258092d508762a5da4936b
Anton Balbelin <balbekinanton@gmail.com>
2020-04-25 23:26:26 +0300

    й

--- resum/js/mr.js
+++ resum/js/mr.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var ws  = window.innerWidth
 	if (ws>=768) {
 		var	h = $('#nw').width();
-		var hp = h - 10;
+		var hp = h;
 		$('.mrmy').css({'margin-left' : hp} );
 	}else{
 		$('.mrmy').css({'margin-left' : 60 } );
